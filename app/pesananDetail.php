fd2cfad6fa60995cdb447bb959ba9c7ade7621dc
salmanurrahma <99798368+salmanurrahma@users.noreply.github.com>
2023-06-12 12:05:37 +0700

    Update pesananDetail.php

--- app/pesananDetail.php
+++ app/pesananDetail.php
@@ -3,16 +3,19 @@
 namespace App;
 use Illuminate\Database\Eloquent\Model;
 class pesananDetail extends Model
+
     
 {
+    
     public function user()
     {
            return $this->belongsTo('app\Barang','user_id','id');
     }
 
-    public function pesanan()
+    public function pesanandetail()
     {
            return $this->belongsTo('app\Pesanan','pesanan_id','id');
+        
     }
 }
 
