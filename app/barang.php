576b341445263b1f36fb2eec89643d4fc4103813
salmanurrahma <99798368+salmanurrahma@users.noreply.github.com>
2023-06-12 11:54:41 +0700

    Update barang.php

--- app/barang.php
+++ app/barang.php
@@ -1,13 +1,12 @@
 <?php
 
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
-
 class barang extends Model
+    
 {
     public function pesanan_detail()
     {
            return $this->hasMany('app\PesananDetail','barang_id','id');
-    }
+   }
 }
